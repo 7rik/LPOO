51c4181df8b9376be3c5dc477a66b07ca49d6250
Lab03 <bezerra.erik2003@gmail.com>
2023-03-23 20:02:16 -0300

    uso de getters e setters

---
+++ src/aulaEncapsulamento/ex_02.java
@@ -0,0 +1,14 @@
+package aulaEncapsulamento;
+
+/* uso de getters e setters*/
+
+public class ex_02 {
+    private float salario;
+
+    public float getSalario() {
+        return salario;
+    }
+    public void setSalario(float valor){
+        salario = valor;
+    }
+}
