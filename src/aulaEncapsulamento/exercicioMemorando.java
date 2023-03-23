6795a6aea4420f7f1133feb4817f59d226deeae6
Lab03 <bezerra.erik2003@gmail.com>
2023-03-23 20:19:00 -0300

    exercício, criando um memorando

---
+++ src/aulaEncapsulamento/exercicioMemorando.java
@@ -0,0 +1,30 @@
+package aulaEncapsulamento;
+
+public class exercicioMemorando {
+    private String assunto, destinatario, signatario;
+
+    public String getAssunto() {
+        return assunto;
+    }
+
+    public String getDestinatario() {
+        return destinatario;
+    }
+
+    public String getSignatario() {
+        return signatario;
+    }
+
+    public void setAssunto(String assunto) {
+        this.assunto = assunto;
+    }
+
+    public void setDestinatario(String destinatario) {
+        this.destinatario = destinatario;
+    }
+
+    public void setSignatario(String signatario) {
+        this.signatario = signatario;
+    }
+}
+
